78707c4d0328a6e3df11c7a79b5b0f6994109290
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-04-10 15:45:39 -0400

    Change names of repo and pakage macros to better namespacing
    Here I changed the names of the macros:
    
    * TRIBITS_DEFINE_PACKAGE_DEPENDENCIES() to
      TRIBITS_PACKAGE_DEFINE_DEPENDENCIES()
    
    * TRIBITS_DEFINE_REPOSITORY_PACKAGES() to TRIBITS_REPOSITORY_DEFINE_PACKAGES()
    
    * TRIBITS_DEFINE_REPOSITORY_TPLS() to TRIBITS_REPOSITORY_DEFINE_TPLS()
    
    This better matches other macros and sorts better.

--- package_arch/UnitTests/MockTrilinos/packages/rbgen/cmake/Dependencies.cmake
+++ package_arch/UnitTests/MockTrilinos/packages/rbgen/cmake/Dependencies.cmake
@@ -37,7 +37,7 @@
 # ************************************************************************
 # @HEADER
 
-TRIBITS_DEFINE_PACKAGE_DEPENDENCIES(
+TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   LIB_REQUIRED_PACKAGES Teuchos
   LIB_OPTIONAL_PACKAGES Epetra EpetraExt
   LIB_REQUIRED_TPLS DUMMY
