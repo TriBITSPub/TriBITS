3ebad3dc91bb40a56e7117f25c93e8268b6b2532
Nico Schlömer <nico.schloemer@gmail.com>
2014-09-12 11:49:35 +0200

    use CMake's WRITE_BASIC_PACKAGE_VERSION_FILE
    This removes the need for our own custom TribitsConfigVersionTemplate.cmake.in.

--- package_arch/TribitsWriteClientExportFiles.cmake
+++ package_arch/TribitsWriteClientExportFiles.cmake
@@ -885,15 +885,16 @@ ENDIF()
   #
   # Configure the version file for ${PROJECT_NAME}
   #
-
-  CONFIGURE_FILE(
-    ${${PROJECT_NAME}_TRIBITS_DIR}/${TRIBITS_CMAKE_INSTALLATION_FILES_DIR}/TribitsConfigVersionTemplate.cmake.in
-    ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake @ONLY)
-
+  INCLUDE(CMakePackageConfigHelpers)
+  WRITE_BASIC_PACKAGE_VERSION_FILE(
+    ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+    VERSION ${${PROJECT_NAME}_VERSION}
+    COMPATIBILITY SameMajorVersion
+    )
   INSTALL(
     FILES ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
     DESTINATION "${${PROJECT_NAME}_INSTALL_LIB_DIR}/cmake/${PROJECT_NAME}"
-  )
+    )
 
 ENDFUNCTION()
 
