1ee0188995973f2874cb6cead5c4a07d91f529bd
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-10-03 15:18:51 -0400

    Change final test regex to not break outer test regex
    There was an evil bug in these unit tests.  The issue is that when I
    added testing testing of the ctest properties, I added a check for
    the final TRIBITS_ADD_ADVANCED_TEST() test criteria.  In doing so
    I printed out "OVERALL FINAL RESULT: TEST PASSED".  The problem is that
    the outer TAAT() command greps for this and if it finds it it says the
    test is passing.  It turns out that I added a new test and changed the
    number of passing tests and it just accepted it.
    
    This commit changes the behavior of TAAT() so that in unit test mode it
    sets the final regex as "OVERALL_FINAL_RESULT: TEST PASSED".  Now the outer
    TAAT() test will only pass if the driver script prints that property.

--- package_arch/UnitTests/TestingFunctionMacro_UnitTests.cmake
+++ package_arch/UnitTests/TestingFunctionMacro_UnitTests.cmake
@@ -1504,8 +1504,12 @@ FUNCTION(UNITEST_TRIBITS_ADD_ADVANCED_TEST_PROPERTIES)
     )
   UNITTEST_COMPARE_CONST(
     TRIBITS_SET_TEST_PROPERTIES_INPUT
-    "PackageA_PackageAddAdvancedTest_basic_cmnd_1_args_0;PROPERTY;REQUIRED_FILES;someCmnd;PackageA_PackageAddAdvancedTest_basic_cmnd_1_args_0;APPEND;PROPERTY;LABELS;PackageA_PackageAddAdvancedTest_basic_cmnd_1_args_0;PROPERTIES;PROCESSORS;1;PackageA_PackageAddAdvancedTest_basic_cmnd_1_args_0;PROPERTIES;PASS_REGULAR_EXPRESSION;OVERALL FINAL RESULT: TEST PASSED"
+    "PackageA_PackageAddAdvancedTest_basic_cmnd_1_args_0;PROPERTY;REQUIRED_FILES;someCmnd;PackageA_PackageAddAdvancedTest_basic_cmnd_1_args_0;APPEND;PROPERTY;LABELS;PackageA_PackageAddAdvancedTest_basic_cmnd_1_args_0;PROPERTIES;PROCESSORS;1;PackageA_PackageAddAdvancedTest_basic_cmnd_1_args_0;PROPERTIES;PASS_REGULAR_EXPRESSION;OVERALL_FINAL_RESULT: TEST PASSED"
     )
+  # NOTE: Above, in unit test mode, TRIBITS_ADD_ADVANCED_TEST() changes is
+  # final pass expression so as to not match the outer run of
+  # TRIBITS_ADD_ADVANCED_TEST() that looks for it.  Otherwise, the outer
+  # TRIBITS_ADD_ADVANCED_TEST() always thinks these unit tests pass!
 
   MESSAGE("Test setting non-integer TIMEOUT with no scaling (not even defined)")
   TRIBITS_ADD_ADVANCED_TEST_UNITTEST_RESET()
@@ -2019,4 +2023,4 @@ MESSAGE("*** Determine final result of all unit tests")
 MESSAGE("***\n")
 
 # Pass in the number of expected tests that must pass!
-UNITTEST_FINAL_RESULT(232)
+UNITTEST_FINAL_RESULT(233)
