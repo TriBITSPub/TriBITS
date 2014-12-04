5807455c0832f2e492e12fde19cff8510eb35646
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-12-04 09:10:51 -0500

    Get test/core/ExampleUnitTests/ working (TriBITS #26)
    This is 175 of the 176 tests that I need to get working again.  Almost there!

--- tribits/examples/TribitsExampleProject/TPLsList.cmake
+++ tribits/examples/TribitsExampleProject/TPLsList.cmake
@@ -1,4 +1,4 @@
 TRIBITS_REPOSITORY_DEFINE_TPLS(
-  MPI  "${${PROJECT_NAME}_TRIBITS_DIR}/tpls/FindTPLMPI.cmake"   PT
+  MPI  "${${PROJECT_NAME}_TRIBITS_DIR}/core/std_tpls/FindTPLMPI.cmake"   PT
   HeaderOnlyTpl  "cmake/tpls/"   PT
   )
