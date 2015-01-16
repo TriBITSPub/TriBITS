f4d606517c35d1a62b72dcff3fb195579bc0c9b7
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-01-16 14:35:43 -0500

    Break off module to find devtools_install path (PHI Kanban #3262 and #3217)
    This will be reused to in install_devtools_UnitTest.py.

--- test/devtools_install/InstallProgramDriver_UnitTests.py
+++ test/devtools_install/InstallProgramDriver_UnitTests.py
@@ -41,15 +41,7 @@
 # Unit testing code for TribitsPackageFilePathUtils.py #
 ######################################################### 
 
-import os
-import sys
-
-devtoolsInstallDir = os.path.join(
-  os.path.dirname(os.path.abspath(__file__)),
-  "../..", "tribits/devtools_install" )
-sys.path = [devtoolsInstallDir] + sys.path
-
-
+from FindDevtoolsInstall import *
 from InstallProgramDriver import *
 import unittest
 
