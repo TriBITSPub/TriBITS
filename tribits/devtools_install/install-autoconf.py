af538cb90a5d50e2218388a3a74a0c9ae02ad929
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-02-04 12:22:11 -0500

    Update install-autoconf.py for updated InstallProgramDriver.py

--- tribits/devtools_install/install-autoconf.py
+++ tribits/devtools_install/install-autoconf.py
@@ -39,18 +39,17 @@
 # ************************************************************************
 # @HEADER
 
+
 #
-# Version info that will change with new versions
+# Defaults
 #
 
-autoconfVersion = "2.69"
-autoconfVersionFull = "2.69"
-
-autoconfTarball = "autoconf-"+autoconfVersionFull+".tar.gz"
-autoconfSrcDir = "autoconf-"+autoconfVersionFull
-autoconfDefaultCheckoutCmnd = \
-  "git clone https://github.com/TriBITSPub/devtools-autoconf-"+autoconfVersion+"-base" \
-  + " autoconf-"+autoconfVersion+"-base" 
+autoconfBaseName = "autoconf"
+autoconfDefaultVersion = "2.69"
+autoconfSupportedVersions = ["2.69"]
+autoconfTarballVersions = {
+  "2.69" : "2.69"
+  }
 
 #
 # Script code
@@ -66,83 +65,107 @@ class AutoconfInstall:
   def __init__(self):
     self.dummy = None
 
-  def getProductName(self):
-    return "autoconf-"+autoconfVersion
-
-  def getBaseDirName(self):
-    return "autoconf-"+autoconfVersion+"-base"
+  #
+  # Called before even knowing the product version
+  #
 
   def getScriptName(self):
     return "install-autoconf.py"
 
-  def getExtraHelpStr(self):
+  def getProductBaseName(self):
+    return autoconfBaseName
+
+  def getProductDefaultVersion(self):
+    return autoconfDefaultVersion
+
+  def getProductSupportedVersions(self):
+    return autoconfSupportedVersions
+
+  #
+  # Called after knowing the product version but before parsing the
+  # command-line.
+  #
+
+  def getProductName(self, version):
+    return autoconfBaseName+"-"+version
+
+  def getBaseDirName(self, version):
+    return autoconfBaseName+"-"+version+"-base"
+
+  def getExtraHelpStr(self, version):
     return """
-The assumed directory structure of the download source provided by the
-command --checkout-cmnd=<checkout-cmnd> is:
+This script builds Autoconf"""+self.getProductName(version)+""" from source compiled with the
+configured C compilers in your path.
+
+NOTE: The assumed directory structure of the download source provided by the
+command --download-cmnd=<download-cmnd> is:
 
    autoconf-<version>-base/
-     autoconf-<version>.tar.gz
-
-ToDo: Allow user to select different autoconf versions.
+     autoconf-<full-version>.tar.gz
 """
 
-  def injectExtraCmndLineOptions(self, clp):
-    clp.add_option(
-      "--checkout-cmnd", dest="checkoutCmnd", type="string",
-      default=autoconfDefaultCheckoutCmnd,
-      help="Command used to check out Autoconf and dependent source." \
-        +"  (Default ='"+autoconfDefaultCheckoutCmnd+"')  WARNING: This will delete" \
-        +" an existing directory if it already exists!")
+  def injectExtraCmndLineOptions(self, clp, version):
+    setStdDownloadCmndOption(self, clp, version)
     clp.add_option(
       "--extra-configure-options", dest="extraConfigureOptions", type="string", \
       default="", \
-      help="Extra options to add to the 'configure' command for "+self.getProductName()+"." \
+      help="Extra options to add to the 'configure' command for "+self.getProductName(version)+"." \
         +"  Note: This does not override the hard-coded configure options." )
 
   def echoExtraCmndLineOptions(self, inOptions):
     cmndLine = ""
-    cmndLine += "  --checkout-cmnd='"+inOptions.checkoutCmnd+"' \\\n"
+    cmndLine += "  --download-cmnd='"+inOptions.downloadCmnd+"' \\\n"
     return cmndLine
+
+  #
+  # Called after parsing the command-line
+  #
     
   def setup(self, inOptions):
     self.inOptions = inOptions
     self.baseDir = os.getcwd()
-    self.autoconfBaseDir = self.baseDir+"/"+self.getBaseDirName()
-    self.autoconfSrcBaseDir = self.autoconfBaseDir+"/"+autoconfSrcDir
+    self.autoconfBaseDir = self.baseDir+"/"+self.getBaseDirName(self.inOptions.version)
+    autoconfVersionFull = autoconfTarballVersions[self.inOptions.version]
+    self.autoconfTarball = "autoconf-"+autoconfVersionFull+".tar.gz"
+    self.autoconfSrcDir = "autoconf-"+autoconfVersionFull
     self.autoconfBuildBaseDir = self.autoconfBaseDir+"/autoconf-build"
     self.scriptBaseDir = getScriptBaseDir()
 
-  def getParallelOpt(self, optName):
-    if self.inOptions.parallel > 0:
-      return " "+optName+str(self.inOptions.parallel)
-    return " "
+  #
+  # Called after setup()
+  #
 
-  def doCheckout(self):
-    removeDirIfExists(self.getBaseDirName(), True)
-    echoRunSysCmnd(self.inOptions.checkoutCmnd)
+  def doDownload(self):
+    removeDirIfExists(self.autoconfBaseDir, True)
+    echoRunSysCmnd(self.inOptions.downloadCmnd)
 
   def doUntar(self):
+    # Find the full name of the source tarball
     echoChDir(self.autoconfBaseDir)
-    echoRunSysCmnd("tar -xzf "+autoconfTarball)
+    echoRunSysCmnd("tar -xzf "+self.autoconfTarball)
 
   def doConfigure(self):
     createDir(self.autoconfBuildBaseDir, True, True)
     echoRunSysCmnd(
-      "../"+autoconfSrcDir+"/configure "+\
+      "../"+self.autoconfSrcDir+"/configure "+\
       " "+self.inOptions.extraConfigureOptions+\
-      " --prefix="+self.inOptions.installDir)
+      " --prefix="+self.inOptions.installDir,
+      extraEnv={"CFLAGS":"-O3"},
+      )
 
   def doBuild(self):
     echoChDir(self.autoconfBuildBaseDir)
-    echoRunSysCmnd("make "+self.getParallelOpt("-j")+self.inOptions.makeOptions)
+    echoRunSysCmnd("make " + getParallelOpt(self.inOptions, "-j") \
+      + self.inOptions.makeOptions)
 
   def doInstall(self):
     echoChDir(self.autoconfBuildBaseDir)
-    echoRunSysCmnd("make "+self.inOptions.makeOptions+" install")
+    echoRunSysCmnd("make " + getParallelOpt(self.inOptions, "-j") \
+      + self.inOptions.makeOptions + " install")
 
   def getFinalInstructions(self):
     return """
-To use the installed version of autoconf-"""+autoconfVersion+""" add the path:
+To use the installed version of autoconf-"""+self.inOptions.version+""" add the path:
 
   """+self.inOptions.installDir+"""/bin
 
