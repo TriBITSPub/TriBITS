812e0ec0f163b0dd4fac7eda0ae17fe4e16e8b27
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-02-04 13:18:58 -0500

    Update install-mpich.py

--- tribits/devtools_install/install-mpich.py
+++ tribits/devtools_install/install-mpich.py
@@ -39,17 +39,17 @@
 # ************************************************************************
 # @HEADER
 
+
 #
-# Version info that will change with new versions
+# Defaults
 #
 
-mpichVersion = "3.1.3"
-
-mpichSrcDir = "mpich-"+mpichVersion
-mpichTarball = mpichSrcDir+".tar.gz"
-mpichDefaultCheckoutCmnd = \
-  "git clone https://github.com/TriBITSPub/devtools-mpich-"+mpichVersion+"-base" \
-  + " mpich-"+mpichVersion+"-base" 
+mpichBaseName = "mpich"
+mpichDefaultVersion = "3.1.3"
+mpichSupportedVersions = ["3.1.3"]
+mpichTarballVersions = {
+  "3.1.3" : "3.1.3"
+  }
 
 #
 # Script code
@@ -65,95 +65,116 @@ class MpichInstall:
   def __init__(self):
     self.dummy = None
 
-  def getProductName(self):
-    return "mpich-"+mpichVersion
-
-  def getBaseDirName(self):
-    return "mpich-"+mpichVersion+"-base"
+  #
+  # Called before even knowing the product version
+  #
 
   def getScriptName(self):
     return "install-mpich.py"
 
-  def getExtraHelpStr(self):
+  def getProductBaseName(self):
+    return mpichBaseName
+
+  def getProductDefaultVersion(self):
+    return mpichDefaultVersion
+
+  def getProductSupportedVersions(self):
+    return mpichSupportedVersions
+
+  #
+  # Called after knowing the product version but before parsing the
+  # command-line.
+  #
+
+  def getProductName(self, version):
+    return mpichBaseName+"-"+version
+
+  def getBaseDirName(self, version):
+    return mpichBaseName+"-"+version+"-base"
+
+  def getExtraHelpStr(self, version):
     return """
-This script builds MPICH """+self.getProductName()+""" from source compiled
-with the configured C/C++ compilers in your path or set by the env vars CC and
-CXX.
+This script builds """+self.getProductName(version)+""" from source compiled
+with the configured C/C++/Fortran compilers in your path or set by the env
+vars CC, CXX, and FC.
 
 NOTE: The assumed directory structure of the download source provided by the
-command --checkout-cmnd=<checkout-cmnd> is:
+command --download-cmnd=<download-cmnd> is:
 
    mpich-<version>-base/
-     mpich-<version>/
-
-ToDo: Allow user to select different mpich versions.
+     mpich-<version>.tar.gz
 """
 
-  def injectExtraCmndLineOptions(self, clp):
-    clp.add_option(
-      "--checkout-cmnd", dest="checkoutCmnd", type="string",
-      default=mpichDefaultCheckoutCmnd,
-      help="Command used to check out MPICH and dependent source." \
-        +"  (Default ='"+mpichDefaultCheckoutCmnd+"')  WARNING: This will delete" \
-        +" an existing directory if it already exists!")
+  def injectExtraCmndLineOptions(self, clp, version):
+    setStdDownloadCmndOption(self, clp, version)
     clp.add_option(
       "--extra-configure-options", dest="extraConfigureOptions", type="string", \
       default="", \
-      help="Extra options to add to the 'configure' command for "+self.getProductName()+"." \
+      help="Extra options to add to the 'configure' command for "+self.getProductName(version)+"." \
         +"  Note: This does not override the hard-coded configure options." )
 
   def echoExtraCmndLineOptions(self, inOptions):
     cmndLine = ""
-    cmndLine += "  --checkout-cmnd='"+inOptions.checkoutCmnd+"' \\\n"
+    cmndLine += "  --download-cmnd='"+inOptions.downloadCmnd+"' \\\n"
     cmndLine += "  --extra-configure-options='"+inOptions.extraConfigureOptions+"' \\\n"
     return cmndLine
+
+  #
+  # Called after parsing the command-line
+  #
     
   def setup(self, inOptions):
     self.inOptions = inOptions
     self.baseDir = os.getcwd()
-    self.mpichBaseDir = self.baseDir+"/"+self.getBaseDirName()
-    self.mpichSrcBaseDir = self.mpichBaseDir+"/"+mpichSrcDir
+    self.mpichBaseDir = self.baseDir+"/"+self.getBaseDirName(self.inOptions.version)
+    mpichVersionFull = mpichTarballVersions[self.inOptions.version]
+    self.mpichTarball = "mpich-"+mpichVersionFull+".tar.gz"
+    self.mpichSrcDir = "mpich-"+mpichVersionFull
     self.mpichBuildBaseDir = self.mpichBaseDir+"/mpich-build"
     self.scriptBaseDir = getScriptBaseDir()
 
-  def getParallelOpt(self, optName):
-    if self.inOptions.parallel > 0:
-      return " "+optName+str(self.inOptions.parallel)
-    return " "
+  #
+  # Called after setup()
+  #
 
-  def doCheckout(self):
-    removeDirIfExists(self.getBaseDirName(), True)
-    echoRunSysCmnd(self.inOptions.checkoutCmnd)
+  def doDownload(self):
+    removeDirIfExists(self.mpichBaseDir, True)
+    echoRunSysCmnd(self.inOptions.downloadCmnd)
 
   def doUntar(self):
+    # Find the full name of the source tarball
     echoChDir(self.mpichBaseDir)
-    echoRunSysCmnd("tar -xzf "+mpichTarball)
+    echoRunSysCmnd("tar -xzf "+self.mpichTarball)
     # NOTE: I found that you have to untar the tarball and can't store the
-    # open source.  Otherwise the timestaps are messed up and it 'make' tries
-    # to recreate some generated files.
+    # open source in the git repo.  Otherwise the timestaps are messed up and
+    # it 'make' tries to recreate some generated files.
 
   def doConfigure(self):
     createDir(self.mpichBuildBaseDir, True, True)
     echoRunSysCmnd(
-      "../"+mpichSrcDir+"/configure " \
-      +" "+self.inOptions.extraConfigureOptions \
-      +" --prefix="+self.inOptions.installDir)
+      "../"+self.mpichSrcDir+"/configure "+\
+      " "+self.inOptions.extraConfigureOptions+\
+      " --prefix="+self.inOptions.installDir,
+      extraEnv={"CFLAGS":"-O3", "CXXFLAGS":"-O3", "FFLAGS":"-O3"},
+      )
 
   def doBuild(self):
     echoChDir(self.mpichBuildBaseDir)
-    echoRunSysCmnd("make "+self.getParallelOpt("-j")+self.inOptions.makeOptions)
+    echoRunSysCmnd("make " + getParallelOpt(self.inOptions, "-j") \
+      + self.inOptions.makeOptions)
 
   def doInstall(self):
     echoChDir(self.mpichBuildBaseDir)
-    echoRunSysCmnd("make "+self.inOptions.makeOptions+" install")
+    echoRunSysCmnd("make " + getParallelOpt(self.inOptions, "-j") \
+      + self.inOptions.makeOptions + " install")
 
   def getFinalInstructions(self):
     return """
-In order to use """+self.getProductName()+""" prepend
+To use the installed version of mpich-"""+self.inOptions.version+""" add the path:
 
-   """+self.inOptions.installDir+"""/bin
+  """+self.inOptions.installDir+"""/bin
 
-to your PATH env variable.
+to your path and that should be it!
 
 Also, you must prepend
 
