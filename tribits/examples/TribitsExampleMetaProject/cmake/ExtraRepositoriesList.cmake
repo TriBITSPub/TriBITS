024b4ea615863d0039e84ea3781a22912946ed47
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-06-04 18:01:20 -0400

    Add TribitsExampleMetaProject (TriBITS #73)
    This pulls in TribitsExampleProject and TribitsExampleProjectAddons as extra
    repos.

---
+++ tribits/examples/TribitsExampleMetaProject/cmake/ExtraRepositoriesList.cmake
@@ -0,0 +1,4 @@
+TRIBITS_PROJECT_DEFINE_EXTRA_REPOSITORIES(
+  TribitsExampleProject         ""  GIT  dummy-git-url  ""   Continuous
+  TribitsExampleProjectAddons   ""  GIT  dummy-git-url  ""   Continuous
+  )
