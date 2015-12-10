25b17b0300a02448d64ba5a17190f999af62fcb1
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-12-10 12:25:19 -0500

    Add install for CMake 3.4.1 (PHI #3904)

--- tribits/devtools_install/install-cmake.py
+++ tribits/devtools_install/install-cmake.py
@@ -46,12 +46,13 @@
 
 cmakeBaseName = "cmake"
 cmakeDefaultVersion = "2.8.11"
-cmakeSupportedVersions = ["2.8.11", "3.1.1", "3.3.2", "3.4.0"]
+cmakeSupportedVersions = ["2.8.11", "3.1.1", "3.3.2", "3.4.0", "3.4.1"]
 cmakeTarballVersions = {
   "2.8.11" : "2.8.11.2",
   "3.1.1" : "3.1.1",
   "3.3.2" : "3.3.2",
-  "3.4.0" : "3.4.0"
+  "3.4.0" : "3.4.0",
+  "3.4.1" : "3.4.1"
   }
 
 # NOTES:
