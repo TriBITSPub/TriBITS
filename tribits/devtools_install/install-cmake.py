7c64ad7dde6168e53515abdf772dc9986ad1a9a1
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-12-15 19:42:59 -0500

    Install CMake 3.3.2 by default instead of 2.8.11 (PHI #3933)
    CMake 3.3.2 has more features and is faster than CMake 2.8.11.  Win, win.

--- tribits/devtools_install/install-cmake.py
+++ tribits/devtools_install/install-cmake.py
@@ -45,7 +45,7 @@
 #
 
 cmakeBaseName = "cmake"
-cmakeDefaultVersion = "2.8.11"
+cmakeDefaultVersion = "3.3.2"
 cmakeSupportedVersions = ["2.8.11", "3.1.1", "3.3.2", "3.4.0", "3.4.1"]
 cmakeTarballVersions = {
   "2.8.11" : "2.8.11.2",
