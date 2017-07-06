19dc733b3532233dee77424ab5f11f398baf1ce0
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-07-06 11:49:18 -0600

    Mention circular dependnecies as possibility in error message
    Just saw this going over this with jfrye.
    
    Had to update test since it greped for the exact error message.
    
    Build/Test Cases Summary
    Enabled Packages:
    Enabled all Packages
    0) MPI_DEBUG => passed: passed=246,notpassed=0 (0.60 min)
    1) SERIAL_RELEASE => passed: passed=246,notpassed=0 (0.61 min)
    2) MPI_DEBUG_CMAKE-3.6.2 => passed: passed=265,notpassed=0 (0.57 min)
    3) SERIAL_RELEASE_CMAKE-3.6.2 => passed: passed=265,notpassed=0 (0.53 min)

--- tribits/core/package_arch/TribitsAdjustPackageEnables.cmake
+++ tribits/core/package_arch/TribitsAdjustPackageEnables.cmake
@@ -135,6 +135,8 @@ FUNCTION(TRIBITS_ABORT_ON_MISSING_PACKAGE   DEP_PKG  PACKAGE_NAME  DEP_PKG_LIST_
     "Error, the package '${DEP_PKG}' is listed as a dependency of the package"
     " '${PACKAGE_NAME}' is in the list '${DEP_PKG_LIST_NAME}' but the package"
     " '${DEP_PKG}' is either not defined or is listed later in the package order."
+    "  This may also be an attempt to create a cicular dependency between"
+    " the packages '${DEP_PKG}' and '${PACKAGE_NAME}' (which is not allowed)."
     "  Check the spelling of '${DEP_PKG}' or see how it is listed in"
     " ${PROJECT_NAME}_PACKAGES_AND_DIRS_AND_CLASSIFICATIONS in relationship to"
     " '${PACKAGE_NAME}'.")
