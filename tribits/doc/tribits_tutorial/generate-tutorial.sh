278bb696f1d86d2c7866954df800e104c226dead
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-07-06 11:36:49 -0600

    Remove ../ to allow latex docs built in base dir (#190)
    Now that the outputs are ignored, it is okay to build these docs in the base
    directory.  (And that is standard latex practice.)

--- tribits/doc/tribits_tutorial/generate-tutorial.sh
+++ tribits/doc/tribits_tutorial/generate-tutorial.sh
@@ -2,5 +2,5 @@
 
 echo "Generating HTML and PDF files ..."
 
-latex  -output-format=pdf ../TribitsTutorialHelloWorld_0.tex
-latex  -output-format=pdf ../TribitsTutorialHelloWorld_1.tex
+latex  -output-format=pdf TribitsTutorialHelloWorld_0.tex
+latex  -output-format=pdf TribitsTutorialHelloWorld_1.tex
