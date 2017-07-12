fcb7ac21910f092877b45025585c91dbae0ba7f7
Jason M. Gates <jmgate@sandia.gov>
2017-07-12 13:45:23 -0600

    Colorize git commands run through gitdist
    When opening a subprocess and passing the stdout to PIPE, the output is
    buffered, losing the colors otherwise seen when running the command from
    the command line.  One solution to keeping the colors is to redirect the
    stdout and stderr of the subprocess to the stdout and stderr of the main
    process.  Calling communicate() then makes the call block until the end
    of the subprocess, otherwise gitdist would continue while the previous
    git command was finishing, leading to interspersed lines of output.

--- tribits/python_utils/gitdist.py
+++ tribits/python_utils/gitdist.py
@@ -785,11 +785,7 @@ def runCmnd(options, cmnd):
   if options.noOpt:
     print(cmnd)
   else:
-    child = subprocess.Popen(cmnd, stdout=subprocess.PIPE).stdout
-    output = child.read()
-    sys.stdout.flush()
-    print(s(output))
-    sys.stdout.flush()
+    subprocess.Popen(cmnd, stdout=sys.stdout, stderr=sys.stderr).communicate()
 
 
 # Determine if a command exists:
