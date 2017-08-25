63cfe39ab8703d0cf69fbd82495091c967a5d99c
Joe Frye <jfrye@sandia.gov>
2017-08-25 13:17:35 -0600

    WIP - Tribits tutorial draft update (#190)
    This is just an update to the tutorial it is not finished but I want
    toback up what I have tot he remote

--- tribits/doc/tribits_tutorial/generate-tutorial.sh
+++ tribits/doc/tribits_tutorial/generate-tutorial.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-echo "Generating HTML and PDF files ..."
+function make_doc {
+if [ -z ${1+x} ]; then
+  echo "no file specified to make_doc"
+  return 1
+elif [ ! -f $1.rst ]; then
+   echo "File not found! $1.rst"
+else
+  echo "Generating HTML and PDF files for ${1} ..."
+  rst2html $1.rst $1.html
+  rst2latex $1.rst $1.tex
+  latex  -output-format=pdf $1.tex
+  rm $1.aux
+  rm $1.out
+  rm $1.log
+  rm $1.toc
+  scp TribitsTutorial_ConvertAProject.pdf cupojoe.srn.sandia.gov:
+fi
+}
 
-latex  -output-format=pdf TribitsTutorialHelloWorld_0.tex
-latex  -output-format=pdf TribitsTutorialHelloWorld_1.tex
+make_doc TribitsTutorial_ConvertAProject
+
