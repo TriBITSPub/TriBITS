a8a1f9fc7da6d97dfba0b9bcce8aa21e4eb15a83
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-09-29 09:28:53 -0600

    Allow customization of number submit return attempts and delay and set new defaults (#230)
    TriBITS was hardcoding the number of submit retry attempts to 25 and the delay
    between attempts as 120 second.  That means that if there is a MySQL error
    (which is happening right now with attempted submits of SPARC build data as
    part of ATDV-84), then it will take 50 minutes to trying to submit before
    moving on!

--- tribits/ctest_driver/TribitsCTestDriverCoreHelpers.cmake
+++ tribits/ctest_driver/TribitsCTestDriverCoreHelpers.cmake
@@ -789,7 +789,9 @@ MACRO(TRIBITS_CTEST_SUBMIT_DRIVER)
   # If using a recent enough ctest with RETRY_COUNT, use it to overcome
   # failed submits:
   SET(retry_args "")
-  SET(retry_args RETRY_COUNT 25 RETRY_DELAY 120)
+  SET(retry_args
+    RETRY_COUNT ${CTEST_SUBMIT_RETRY_COUNT}
+    RETRY_DELAY ${CTEST_SUBMIT_RETRY_DELAY})
   MESSAGE("info: using retry_args='${retry_args}' for _ctest_submit call")
 
   # Call the original CTEST_SUBMIT and pay attention to its RETURN_VALUE:
