3d2c2ef5a8981486efaac8707eb8a604624b53da
Joe Frye <jfrye@sandia.gov>
2017-10-04 10:33:03 -0600

    WIP: more tutorial

--- tribits/doc/tribits_tutorial/generate-tutorial.sh
+++ tribits/doc/tribits_tutorial/generate-tutorial.sh
@@ -4,10 +4,11 @@ function make_doc {
 if [ -z ${1+x} ]; then
   echo "no file specified to make_doc"
   return 1
-elif [ ! -f $1.rst ]; then
+elif [ ! -f src/$1.rst ]; then
    echo "File not found! $1.rst"
 else
   echo "Generating HTML and PDF files for ${1} ..."
+  cp src/$1.rst .
   rst2html $1.rst $1.html
   rst2latex $1.rst $1.tex
   latex  -output-format=pdf $1.tex
@@ -15,10 +16,11 @@ else
   rm $1.out
   rm $1.log
   rm $1.toc
+  rm $1.rst
   scp $1.pdf cupojoe.srn.sandia.gov:
 fi
 }
 
 make_doc TribitsTutorial_ConvertAProject
-make_doc TribitsTutorial_BeyondHelloWorld
+make_doc TribitsTutorial_Dependencies
 make_doc TribitsTutorial_HelloWorld
