44b79407cf6c9e6a4f29f65bf043ea4d837ff9bd
Roscoe A. Bartlett <rabartl@sandia.gov>
2019-05-21 13:37:06 -0600

    Remove debug print statement left over from trilinos/trilinos#2689, ATDV-156

--- tribits/core/package_arch/TribitsGlobalMacros.cmake
+++ tribits/core/package_arch/TribitsGlobalMacros.cmake
@@ -2934,8 +2934,6 @@ MACRO(TRIBITS_SETUP_FOR_INSTALLATION)
 
   ADVANCED_SET(${PROJECT_NAME}_INSTALL_PBP_RUNNER "" CACHE FILEPATH
     "Program used to run cmake -P cmake_pbp_install.cmake to change user for 'install_package_by_package' target")
-  PRINT_VAR(${PROJECT_NAME}_INSTALL_PBP_RUNNER)
-
 
   ADD_CUSTOM_TARGET(install_package_by_package
    ${${PROJECT_NAME}_INSTALL_PBP_RUNNER}
