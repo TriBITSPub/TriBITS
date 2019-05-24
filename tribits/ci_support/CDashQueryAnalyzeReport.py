c2b11deb2ebb2d187288e8c632677ca41b06f308
Roscoe A. Bartlett <rabartl@sandia.gov>
2019-05-24 15:18:48 -0600

    Remove old debug print statements

--- tribits/ci_support/CDashQueryAnalyzeReport.py
+++ tribits/ci_support/CDashQueryAnalyzeReport.py
@@ -1248,8 +1248,8 @@ def checkCDashTestDictsAreSame(testDict_1, testDict_1_name,
     time_2 = testDict_2['time'] 
     rel_err = abs(time_1 - time_2) / ( (time_1 + time_2 + 1e-5)/2.0 )
     rel_err_max = 1.0  # ToDo: Make this adjustable?
-    print("rel_err = "+str(rel_err))
-    print("rel_err_max = "+str(rel_err_max))
+    #print("rel_err = "+str(rel_err))
+    #print("rel_err_max = "+str(rel_err_max))
     if rel_err <= rel_err_max:
       testDict_1_copy.pop('time', None)
       testDict_2_copy.pop('time', None)
