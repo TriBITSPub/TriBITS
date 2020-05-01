b60530c566b83e6b014aa877a975fde5b537b8a7
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-05-01 10:38:51 -0600

    Add exact env and do-configure script I use to drive TriBITS dev on crf450
    Now that we have had to disable TravisCI due to SNL policy, these local tests
    is all we have.

---
+++ dev_testing/crf450/mpi-debug/load-env.sh
@@ -0,0 +1,4 @@
+#export PATH=/home/vera_env/common_tools/cmake-3.17.0/bin:${PATH}
+export PATH=/home/vera_env/common_tools/ccache-3.7.9/bin:${PATH}
+umask g-rwx,o-rwx
+export TribitsExMetaProj_GIT_URL_REPO_BASE=git@github.com:tribits/
