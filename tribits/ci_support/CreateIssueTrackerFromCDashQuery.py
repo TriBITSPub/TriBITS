00a0f42ad6b304658317479b879b459979bd6224
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-12-14 16:04:55 -0700

    Initial CreateIssueTrackerFromCDashQuery.py (ATDV-403)
    This is being used with the
    TrilinosATDMStatus/create_trilinos_github_test_failure_issue.py script and was
    developed very fast with no unit tests.
    
    Some basic regression tests will be added next.

---
+++ tribits/ci_support/CreateIssueTrackerFromCDashQuery.py
@@ -0,0 +1,216 @@
+import sys
+import pprint
+
+import CDashQueryAnalyzeReport as CDQAR
+import cdash_build_testing_date as CDBTD
+
+g_pp = pprint.PrettyPrinter(indent=2)
+
+
+# Class that is used to create issue trackers for nonpassing tests downloaded
+# from a CDash queryTests.php query.
+#
+# To make this general, the user has to create a class object called
+# 'issueTrackerInfoConsumer' in this class that supports the following
+# functions:
+#
+# issueTrackerInfoConsumer.acceptIssueTrackerData(nonpassingTestsUrl, uniqNonpassingTestsLOD,
+#   buildnameList, testnameList, testHistoryHtmlTableTextText ):
+#
+# This function is given data about the nonpasing tests and the object
+# 'issueTrackerInfoConsumeracceptIssueTrackerData' is expected to use it to
+# create the issue issue tracker text.  The only assumption of any formatting
+# is in the 'testHistoryHtmlTableText' argument.  But the object
+# 'issueTrackerInfoConsumer' is free to ignore that table if it can't deal
+# with HTML formatting.
+#
+# issueTrackerInfoConsumer.getIssueTrackerText():
+#
+# This function is expected to return the created text for the issue tracker
+# given the data provided in the above 'acceptIssueTrackerData()' function.
+# The format of this text can be anything such as Markdown, RestructredText,
+# JIRA Issue Markup, etc.  The only assumption about formatted is in the HTML
+# table passed in the arugment 'testHistoryHtmlTableText'
+# 'acceptIssueTrackerData()' function.
+#
+# ToDo: Finish documentation!
+#
+class CreateIssueTrackerFromCDashQueryDriver:
+
+
+  def __init__(self, issueTrackerInfoConsumer, cdashProjectStartTimeUtc=None,
+      usageHelp="", issueTrackerUrlTemplate="", issueTrackerTemplate="",
+    ):
+    self.issueTrackerInfoConsumer = issueTrackerInfoConsumer
+    self.cdashProjectStartTimeUtc = cdashProjectStartTimeUtc
+    self.usageHelp = usageHelp
+    self.issueTrackerUrlTemplate = issueTrackerUrlTemplate
+    self.issueTrackerTemplate = issueTrackerTemplate
+    self.options = None
+
+
+  def runDriver(self):
+
+    self.getCmndLineOptions()
+
+    nonpassingTestsLOD = self.downloadNonpassingTestsData()
+
+    uniqNonpassingTestsLOD = getUniqueTestsListOfDicts(nonpassingTestsLOD)
+
+    (testnameList, buildnameList) = getTestnameAndBuildnameLists(uniqNonpassingTestsLOD)
+
+    testingDayStartNonpassingDate = getTestingDayStartNonpassingDate(
+      nonpassingTestsLOD, self.options.cdashProjectStartTimeUtc)
+    #print("testingDayStartNonpassingDate = "+testingDayStartNonpassingDate)
+
+    testHistoryHtmlTableText = ""  # ToDo: Implement!
+
+    self.issueTrackerInfoConsumer.acceptIssueTrackerData(
+      summaryLine=self.options.summaryLine,
+      testingDayStartNonpassingDate=testingDayStartNonpassingDate,
+      nonpassingTestsUrl=self.options.nonpassingTestsUrl,
+      uniqNonpassingTestsLOD=uniqNonpassingTestsLOD,
+      buildnameList=buildnameList,
+      testnameList=testnameList,
+      testHistoryHtmlTableText=testHistoryHtmlTableText,
+      )
+
+    issueTrackerText = \
+      self.issueTrackerInfoConsumer.getIssueTrackerText()
+
+    if self.options.newIssueTrackerFile:
+      print("\nWriting out '"+self.options.newIssueTrackerFile+"'")
+      with open(self.options.newIssueTrackerFile, 'w') as fileHandle:
+        fileHandle.write(issueTrackerText)
+
+    if self.options.newTestsWithIssueTrackersFile:
+      print("\nWriting out '"+self.options.newTestsWithIssueTrackersFile+"'")
+      csvFileStruct = CDQAR.writeTestsLODToCsvFileStructure(uniqNonpassingTestsLOD,
+        self.issueTrackerUrlTemplate, self.issueTrackerTemplate )
+      with open(self.options.newTestsWithIssueTrackersFile, 'w') as csvFile:
+        csvFile.write(CDQAR.writeCsvFileStructureToStr(csvFileStruct))
+
+
+  def injectExtraCmndLineOptions(self, clp):
+    clp.add_argument(
+      "--nonpassing-tests-url", dest="nonpassingTestsUrl", default="",
+      help="Full CDash queryTest.php URL for the list of nonpassing tests over a"\
+        +" time range given as 'begin' and 'end' fields [Required]." )
+    clp.add_argument(
+      "--cdash-project-start-time", dest="cdashProjectStartTimeUtc", default="",
+      help="Starting time for the CDash testing day in 'hh:mm' in UTC."\
+        + " Check the CDash project settings for the testing day start time." )
+    clp.add_argument(
+      "--summary-line", dest="summaryLine", default="",
+      help="The summary line text for the issue tracker [Required]." )
+    clp.add_argument(
+      "--new-issue-tracker-file", dest="newIssueTrackerFile", default="",
+      help="File created with the new issue tracker text if"\
+        +" specified [Default empty '']." )
+    clp.add_argument(
+      "--new-tests-with-issue-trackers-file", dest="newTestsWithIssueTrackersFile",
+      default="",
+      help="CSV file created with entries for the list of nonpassing tests "\
+        +" if specified [Default empty '']." )
+
+
+  def getCmndLineOptions(self):
+    from argparse import ArgumentParser, RawDescriptionHelpFormatter
+    clp = ArgumentParser(
+      description=self.usageHelp,
+      formatter_class=RawDescriptionHelpFormatter)
+    self.injectExtraCmndLineOptions(clp)
+    self.options = clp.parse_args(sys.argv[1:])
+    self.postReadFixupCommandlineOptions()
+    self.validateCommandLineOptions()
+
+
+  def postReadFixupCommandlineOptions(self):
+    if (self.options.cdashProjectStartTimeUtc == "") \
+        and self.cdashProjectStartTimeUtc \
+      :
+      self.options.cdashProjectStartTimeUtc = self.cdashProjectStartTimeUtc
+
+
+  def validateCommandLineOptions(self):
+    if self.options.nonpassingTestsUrl == "":
+      print("Error, the argument --nopassing-tests-url is required (see --help)")
+      sys.exit(1)
+    if self.options.cdashProjectStartTimeUtc == "":
+      print("Error, the argument --cdash-project-start-time is required (see --help)")
+      sys.exit(1)
+    if self.options.summaryLine == "":
+      print("Error, the argument --summary-line is required (see --help)")
+      sys.exit(1)
+
+
+  def downloadNonpassingTestsData(self):
+    apiQueryTestsUrl = self.options.nonpassingTestsUrl.replace(
+      "/queryTests.php", "/api/v1/queryTests.php")
+    nonpassingTestsLOD = \
+       CDQAR.downloadTestsOffCDashQueryTestsAndFlatten(apiQueryTestsUrl)
+    return nonpassingTestsLOD
+
+
+
+
+#
+# Nonmember functions
+#
+
+
+def getUniqueTestsListOfDicts(cdashTestsLOD):
+  # Get a dict that has just unique keys 'site', 'buildName' and 'testname' values
+  uniqTestDict = {}
+  for testDict in cdashTestsLOD:
+    site = testDict.get('site')
+    buildName = testDict.get('buildName')
+    testname = testDict.get('testname')
+    uniqTestDict.update( {
+      site+"_"+buildName+"_"+testname :
+        {'site':site, 'buildName':buildName, 'testname':testname}
+      } )
+  # Get a flat last of test dicts
+  uniqNonpassingTestsLOD= []
+  for key, value in uniqTestDict.iteritems():
+    uniqNonpassingTestsLOD.append(value)
+  uniqNonpassingTestsLOD = CDQAR.sortAndLimitListOfDicts(uniqNonpassingTestsLOD,
+    CDQAR.getDefaultTestDictsSortKeyList())
+  return uniqNonpassingTestsLOD
+
+
+def getTestnameAndBuildnameLists(uniqNonpassingTestsLOD):
+  testnameSet = set()
+  buildnameSet = set()
+  for testDict in uniqNonpassingTestsLOD:
+    testnameSet.add(testDict.get('testname'))
+    buildnameSet.add(testDict.get('buildName'))
+  testnameList = extractListAndSort(testnameSet)
+  buildnameList = extractListAndSort(buildnameSet)
+  return (testnameList, buildnameList) 
+
+
+def extractListAndSort(setIn):
+  listOut = []
+  for item in setIn: listOut.append(item)
+  listOut.sort()
+  return listOut
+
+
+def getTestingDayStartNonpassingDate(nonpassingTestsLOD, cdashProjectStartTimeUtcStr):
+  cdashProjectStartTimeUtcStrTD = CDBTD.getProjectTestingDayStartTimeDeltaFromStr(
+    cdashProjectStartTimeUtcStr)
+  testingDayStartNonpassingDate = None
+  for testDict in nonpassingTestsLOD:
+    #print("")
+    #g_pp.pprint(testDict)
+    buildStartTime = testDict.get('buildstarttime')
+    #print("buildStartTime = "+str(buildStartTime))
+    testingDate = CDBTD.getTestingDayDateFromBuildStartTimeStr(buildStartTime,
+      cdashProjectStartTimeUtcStrTD)
+    if (testingDayStartNonpassingDate==None) \
+        or (testingDate  < testingDayStartNonpassingDate) \
+      :
+      testingDayStartNonpassingDate = testingDate
+    #print("testingDayStartNonpassingDate = "+str(testingDayStartNonpassingDate))
+    return testingDayStartNonpassingDate
