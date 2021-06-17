f61da2711eec8ce609fa3865f848853897286326
Roscoe A. Bartlett <rabartl@sandia.gov>
2021-06-17 16:00:32 -0600

    Also lower-case cmake code in *.cmake.in files (#274)

--- tribits/refactoring/lower-case-cmake-tree.sh
+++ tribits/refactoring/lower-case-cmake-tree.sh
@@ -7,5 +7,5 @@
 _SCRIPT_DIR=`echo $0 | sed "s/\(.*\)\/lower-case-cmake-tree[.]sh/\1/g"`
 baseDir=$1
 find ${baseDir} -type f \
-  \( -name CMakeLists.txt -or -name "*.cmake" -or -name "*.rst" \) \
+  \( -name CMakeLists.txt -or -name "*.cmake" -or -name "*.cmake.in" -or -name "*.rst" \) \
   -exec $_SCRIPT_DIR/../python_utils/lower_case_cmake.py {} ';'
