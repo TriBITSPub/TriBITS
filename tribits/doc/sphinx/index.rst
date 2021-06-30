188b399102a0865c37232d9c5ba0c56ddb0221eb
Marcin Wróbel <marcinwrobel1986@protonmail.com>
2021-06-30 16:32:57 +0200

    #386: - added sphinx setup - added script for setting up relative paths

---
+++ tribits/doc/sphinx/index.rst
@@ -0,0 +1,10 @@
+Welcome to TriBITS's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+   build_ref_rst
+   users_guide_rst
+   maintainers_guide_rst
---
+++ tribits/doc/sphinx/sphinx_rst_generator.py
@@ -0,0 +1,141 @@
+import os
+import subprocess
+import sys
+
+try:
+    doc_path = f"{os.sep}".join(os.path.abspath(__file__).split(os.sep)[:-2])
+    sys.path.append(doc_path)
+    project_path = f"{os.sep}".join(os.path.abspath(__file__).split(os.sep)[:-4])
+    sys.path.append(project_path)
+except Exception as e:
+    print(f"Can not add project path to system path! Exiting!\nERROR: {e}")
+    exit(1)
+
+
+class SphinxRstGenerator:
+    def __init__(self):
+        self.paths = {
+            'mainteiners_guide': {
+                'src': os.path.join(doc_path, 'guides', 'maintainers_guide', 'TribitsMaintainersGuide.rst'),
+                'src_path': os.path.join(doc_path, 'guides', 'maintainers_guide'),
+                'final_path': os.path.join(doc_path, 'sphinx', 'maintainers_guide_rst.rst')},
+            'users_guide': {
+                'src': os.path.join(doc_path, 'guides', 'users_guide', 'TribitsUsersGuide.rst'),
+                'src_path': os.path.join(doc_path, 'guides', 'users_guide'),
+                'final_path': os.path.join(doc_path, 'sphinx', 'users_guide_rst.rst')},
+            'build_ref': {
+                'src': os.path.join(doc_path, 'build_ref', 'TribitsBuildReference.rst'),
+                'src_path': os.path.join(doc_path, 'build_ref'),
+                'final_path': os.path.join(doc_path, 'sphinx', 'build_ref_rst.rst')}}
+        self.sphinx_path = os.path.abspath(os.path.join(doc_path, 'sphinx'))
+        self.build_docs()
+
+    @staticmethod
+    def build_docs() -> None:
+        """ Builds TriBITS documentation based on shell scripts
+        """
+        build_script_path = os.path.join(doc_path, 'build_docs.sh')
+        current_working_dir = os.path.split(build_script_path)[0]
+        subprocess.call(build_script_path, cwd=current_working_dir)
+
+    @staticmethod
+    def is_rst_file(file_path: str) -> bool:
+        """ Checks if file_path has .rst extension and if file_path is a file
+        """
+        if os.path.splitext(file_path)[-1] == '.rst' and os.path.isfile(file_path):
+            return True
+        return False
+
+    @staticmethod
+    def save_rst(file_path: str, file_content: str) -> None:
+        """ Saves .rst file with given pathh and content
+        """
+        with open(file_path, 'w') as dest_file:
+            dest_file.write(file_content)
+
+    @staticmethod
+    def get_src_path_from_parent() -> str:
+        pass
+
+    def generate_rst(self, source_file: str, final_path: str = None, src_path: str = None,
+                     start_path: str = None) -> list:
+        """ Generate corect links in .rst files, so Sphinx can find them
+        """
+        if final_path is None:
+            overwrite_source = True
+        else:
+            overwrite_source = False
+
+        if src_path is None:
+            src_file_path = self.get_src_path_from_parent()
+        else:
+            src_file_path = src_path
+
+        file_content, includes = self.change_paths_and_get_includes(source_file=source_file,
+                                                                    src_file_path=src_file_path, start_path=start_path)
+
+        if overwrite_source:
+            self.save_rst(file_path=source_file, file_content=file_content)
+        else:
+            self.save_rst(file_path=final_path, file_content=file_content)
+
+        return includes
+
+    def change_paths_and_get_includes(self, source_file: str, src_file_path: str, start_path: str) -> tuple:
+        """ Changes paths in source file, to be relative to sphinx_path or parent .rst document.
+            Returns a tuple with .rst file content and includes(absolute_path, relative_to)
+        """
+        with open(source_file, 'r') as src_file:
+            source_file_str = src_file.read()
+            source_file_list = list()
+            include_file_list = list()
+            for line in source_file_str.split('\n'):
+                splitted_line = line.split()
+                if 'include::' in splitted_line:
+                    incl_index = splitted_line.index('include::')
+                    path_index = incl_index + 1
+                    if len(splitted_line) > path_index:
+                        new_line = splitted_line[:path_index]
+                        abs_path = os.path.abspath(os.path.join(src_file_path, splitted_line[path_index]))
+                        if self.is_rst_file(file_path=abs_path):
+                            include_file_list.append((abs_path, src_file_path))
+                        rel_path_from_sphinx_dir = os.path.relpath(path=abs_path, start=start_path)
+                        new_line.append(rel_path_from_sphinx_dir)
+                        new_line = ' '.join(new_line)
+                        source_file_list.append(new_line)
+                    else:
+                        source_file_list.append(line)
+                else:
+                    source_file_list.append(line)
+            abs_path_str = '\n'.join(source_file_list)
+
+        return abs_path_str, include_file_list
+
+    def main(self):
+        child_rst = list()
+        for doc_name, sources in self.paths.items():
+            includes = self.generate_rst(source_file=sources.get('src'), src_path=sources.get('src_path'),
+                                         final_path=sources.get('final_path'), start_path=self.sphinx_path)
+            child_rst.extend(includes)
+
+        grand_child_rst = list()
+        for child in child_rst:
+            includes_grand = self.generate_rst(source_file=child[0], src_path=os.path.split(child[0])[0],
+                                               start_path=self.sphinx_path)
+            grand_child_rst.extend(includes_grand)
+
+        grand_grand_child_rst = list()
+        for grand_child in grand_child_rst:
+            includes_grand_grand = self.generate_rst(source_file=grand_child[0],
+                                                     src_path=os.path.split(grand_child[0])[0],
+                                                     start_path=self.sphinx_path)
+            grand_grand_child_rst.extend(includes_grand_grand)
+
+        if not grand_grand_child_rst:
+            print('DONE! ALL GOOD!')
+        else:
+            print('NOT DONE!')
+
+
+if __name__ == '__main__':
+    SphinxRstGenerator().main()
