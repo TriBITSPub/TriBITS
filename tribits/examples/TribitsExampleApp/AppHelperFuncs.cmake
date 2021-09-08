636fb7ec192e2895489cbd4ca16ea0fb78fc6840
Roscoe A. Bartlett <rabartl@sandia.gov>
2021-09-08 10:14:55 -0600

    Update TribitsExampleApp to use find_package(<Package> ...) (#299)
    I added a test for this as well.  This tests that case and shows you don't
    really need the top-level project find_package() command.  Finding things at
    the package-level is more flexible as it gives greater freedom in how packages
    are built and installed.

--- tribits/examples/TribitsExampleApp/AppHelperFuncs.cmake
+++ tribits/examples/TribitsExampleApp/AppHelperFuncs.cmake
@@ -7,33 +7,79 @@ macro(getTribitsExProjStuff)
     "Set to TRUE to find individual packages and OFF to find project TribitsExProj")
 
   if (${PROJECT_NAME}_FIND_INDIVIDUAL_PACKAGES)
-    message(FATAL_ERROR "ToDo: Implement!")
+    getTribitsExProjStuffByPackage()
   else()
-    getTribitsExProjProjectStuff()
+    getTribitsExProjStuffByProject()
   endif()
 
 endmacro()
 
 
+# Get TribitsExProj stuff with find_package(<Package>) for each
+# package/component.
+#
+macro(getTribitsExProjStuffByPackage)
+
+  # Find each package and gather up all the include dirs and libs for each
+  # package and their TPLs and preserve the order.
+  set(APP_DEPS_PACKAGE_INCLUDE_DIRS "")
+  set(APP_DEPS_TPL_INCLUDE_DIRS "")
+  set(APP_DEPS_PACKAGE_LIBRARIES "")
+  set(APP_DEPS_TPL_LIBRARIES "")
+  foreach (packageName IN LISTS ${PROJECT_NAME}_USE_COMPONENTS)
+    find_package(${packageName} REQUIRED)
+    message("Found ${packageName}!")
+    list(APPEND APP_DEPS_PACKAGE_INCLUDE_DIRS ${${packageName}_INCLUDE_DIRS})
+    list(APPEND APP_DEPS_TPL_INCLUDE_DIRS ${${packageName}_TPL_INCLUDE_DIRS})
+    list(APPEND APP_DEPS_PACKAGE_LIBRARIES ${${packageName}_LIBRARIES})
+    list(APPEND APP_DEPS_TPL_LIBRARIES ${${packageName}_TPL_LIBRARIES})
+    #print_var(APP_DEPS_PACKAGE_INCLUDE_DIRS)
+    #print_var(APP_DEPS_TPL_INCLUDE_DIRS)
+    #print_var(APP_DEPS_PACKAGE_LIBRARIES)
+    #print_var(APP_DEPS_TPL_LIBRARIES)
+  endforeach()
+
+  # Get the full list of include dirs and libs
+  set(APP_DEPS_INCLUDE_DIRS
+    ${APP_DEPS_PACKAGE_INCLUDE_DIRS} ${APP_DEPS_TPL_INCLUDE_DIRS})
+  set(APP_DEPS_LIBRARIES
+    ${APP_DEPS_PACKAGE_LIBRARIES} ${APP_DEPS_TPL_LIBRARIES})
+  #print_var(APP_DEPS_INCLUDE_DIRS)
+  #print_var(APP_DEPS_LIBRARIES)
+
+  # Remove duplicates
+  list(REVERSE APP_DEPS_INCLUDE_DIRS)
+  list(REMOVE_DUPLICATES APP_DEPS_INCLUDE_DIRS)
+  list(REVERSE APP_DEPS_INCLUDE_DIRS)
+  print_var(APP_DEPS_INCLUDE_DIRS)
+  list(REVERSE APP_DEPS_LIBRARIES)
+  list(REMOVE_DUPLICATES APP_DEPS_LIBRARIES)
+  list(REVERSE APP_DEPS_LIBRARIES)
+  print_var(APP_DEPS_LIBRARIES)
+
+endmacro()
+
+
+
 # Get TribitsExProj stuff from find_package(TribitsExProj)
 #
-macro(getTribitsExProjProjectStuff)
+macro(getTribitsExProjStuffByProject)
 
   find_package(TribitsExProj REQUIRED COMPONENTS ${${PROJECT_NAME}_USE_COMPONENTS})
 
-  MESSAGE("\nFound TribitsExProj!  Here are the details: ")
-  MESSAGE("   TribitsExProj_DIR = ${TribitsExProj_DIR}")
-  MESSAGE("   TribitsExProj_VERSION = ${TribitsExProj_VERSION}")
-  MESSAGE("   TribitsExProj_PACKAGE_LIST = ${TribitsExProj_PACKAGE_LIST}")
-  MESSAGE("   TribitsExProj_LIBRARIES = ${TribitsExProj_LIBRARIES}")
-  MESSAGE("   TribitsExProj_INCLUDE_DIRS = ${TribitsExProj_INCLUDE_DIRS}")
-  MESSAGE("   TribitsExProj_LIBRARY_DIRS = ${TribitsExProj_LIBRARY_DIRS}")
-  MESSAGE("   TribitsExProj_TPL_LIST = ${TribitsExProj_TPL_LIST}")
-  MESSAGE("   TribitsExProj_TPL_INCLUDE_DIRS = ${TribitsExProj_TPL_INCLUDE_DIRS}")
-  MESSAGE("   TribitsExProj_TPL_LIBRARIES = ${TribitsExProj_TPL_LIBRARIES}")
-  MESSAGE("   TribitsExProj_TPL_LIBRARY_DIRS = ${TribitsExProj_TPL_LIBRARY_DIRS}")
-  MESSAGE("   TribitsExProj_BUILD_SHARED_LIBS = ${TribitsExProj_BUILD_SHARED_LIBS}")
-  MESSAGE("End of TribitsExProj details\n")
+  message("\nFound TribitsExProj!  Here are the details: ")
+  message("   TribitsExProj_DIR = ${TribitsExProj_DIR}")
+  message("   TribitsExProj_VERSION = ${TribitsExProj_VERSION}")
+  message("   TribitsExProj_PACKAGE_LIST = ${TribitsExProj_PACKAGE_LIST}")
+  message("   TribitsExProj_LIBRARIES = ${TribitsExProj_LIBRARIES}")
+  message("   TribitsExProj_INCLUDE_DIRS = ${TribitsExProj_INCLUDE_DIRS}")
+  message("   TribitsExProj_LIBRARY_DIRS = ${TribitsExProj_LIBRARY_DIRS}")
+  message("   TribitsExProj_TPL_LIST = ${TribitsExProj_TPL_LIST}")
+  message("   TribitsExProj_TPL_INCLUDE_DIRS = ${TribitsExProj_TPL_INCLUDE_DIRS}")
+  message("   TribitsExProj_TPL_LIBRARIES = ${TribitsExProj_TPL_LIBRARIES}")
+  message("   TribitsExProj_TPL_LIBRARY_DIRS = ${TribitsExProj_TPL_LIBRARY_DIRS}")
+  message("   TribitsExProj_BUILD_SHARED_LIBS = ${TribitsExProj_BUILD_SHARED_LIBS}")
+  message("End of TribitsExProj details\n")
 
   # Make sure to use same compilers and flags as TribitsExProj
   set(CMAKE_CXX_COMPILER ${TribitsExProj_CXX_COMPILER} )
@@ -79,4 +125,9 @@ function(appendTestDepsStr componentName depsStrOut str)
     endif()
   endif()
   set(${depsStrOut} "${depsStr}" PARENT_SCOPE)
+endfunction()
+
+
+function(print_var varName)
+  message("-- ${varName} = '${${varName}}'")
 endfunction()
