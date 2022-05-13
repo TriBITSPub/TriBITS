d4303762cc2be6a35606826b5d8875606e652cc5
Roscoe A. Bartlett <rabartl@sandia.gov>
2022-05-13 07:54:47 -0600

    TribitsExampleProject2: Remove hacked dependencies on indirect TPLs (#63, #299)
    This shows that the new TPL dependencies actually work to bring in indirect
    TPL dependencies and create the proper link lines.  If you comment out the
    `set(<tplName>_LIB_ENABLED_DEPENDENCIES ...)` statements in
    `TribitsExampleProject2/TPLsList.cmake`, you will see failing tests.

--- tribits/examples/TribitsExampleProject2/packages/package3/cmake/Dependencies.cmake
+++ tribits/examples/TribitsExampleProject2/packages/package3/cmake/Dependencies.cmake
@@ -2,7 +2,5 @@ tribits_package_define_dependencies(
   LIB_REQUIRED_PACKAGES  Package1
   LIB_OPTIONAL_PACKAGES  Package2
   LIB_REQUIRED_TPLS  Tpl2
-    Tpl1  # ToDo: Remove this line once TPL dependencies are implemented! (#299)
   LIB_OPTIONAL_TPLS  Tpl4
-    Tpl3  # ToDo: Remove this line once TPL dependencies are implemented! (#299)
   )
