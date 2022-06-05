1738ccdd861e0da6bfaa683d4ae5eeddea643971
Roscoe A. Bartlett <rabartl@sandia.gov>
2022-06-04 18:36:03 -0600

    Produce CDash URLs to all builds and nonpassing tests for same repo version (#483)
    This makes it easy to click on links to these other CDash URLs to see what is
    happening with all of the builds for the same repo version.
    
    The main target is for the GitHub Actions drivers for TriBITS testing.  This
    makes it easy to get at those results.
    
    As part of this, I also renamed the function
    tribits_get_build_url_and_write_to_file() to tribits_get_cdash_build_url() and
    removed the ability to write to a file.  (We just don't need that anymore and
    it is trivial to write a string to a file.)

--- tribits/ctest_driver/TribitsGetCDashUrlsInsideCTestS.cmake
+++ tribits/ctest_driver/TribitsGetCDashUrlsInsideCTestS.cmake
@@ -43,16 +43,280 @@ include(TribitsParseArgumentsHelpers)
 include(TribitsReadTagFile)
 
 
-# @FUNCTION: tribits_get_build_url_and_write_to_file()
+################################################################################
 #
-# Construct the build URL on CDash given the site name, buildname, and
-# buildstamp (take from the TAG file) from inside of a running ctest -S
+# This module contains functions for constructing CDash URLs to build and test
+# results from inside of a CTest -S script.
+#
+################################################################################
+
+
+# @FUNCTION: tribits_get_cdash_results_string_and_write_to_file()
+#
+# Calls `tribits_get_cdash_results_urls_string()`_ and then writes the CDash
+# URLs to a file.
+#
+# Usage::
+#
+#   tribits_get_cdash_results_string_and_write_to_file(
+#     [CDASH_RESULTS_STRING_OUT <cdashResultsStringOut>]
+#     [CDASH_RESULTS_FILE_OUT <cdashResultsFileOut>]
+#     )
+#
+function(tribits_get_cdash_results_string_and_write_to_file)
+  # Parse args
+  cmake_parse_arguments( PARSE_ARGV 0
+    PARSE "" "" # prefix, options, one_value_keywords
+    "CDASH_RESULTS_STRING_OUT;CDASH_RESULTS_FILE_OUT" # multi_value_keywords
+    )
+  tribits_check_for_unparsed_arguments(PARSE)
+  tribits_assert_parse_arg_zero_or_one_value(PARSE  CDASH_RESULTS_STRING_OUT
+    CDASH_RESULTS_FILE_OUT )
+  # Get and set CDash results URL
+  tribits_get_cdash_results_urls_string(cdashResultsString)
+  if (PARSE_CDASH_RESULTS_STRING_OUT)
+    set(${PARSE_CDASH_RESULTS_STRING_OUT} "${cdashResultsString}" PARENT_SCOPE)
+  endif()
+  if (PARSE_CDASH_RESULTS_FILE_OUT)
+    file(WRITE "${PARSE_CDASH_RESULTS_FILE_OUT}" "${cdashResultsString}")
+  endif()
+endfunction()
+
+
+# @FUNCTION: tribits_get_cdash_results_urls_string()
+#
+# Call `tribits_get_cdash_results_urls()`_ and then construct a CDash URLs
+# string fit for printing.
+#
+# Usage::
+#
+#   tribits_get_cdash_results_urls_string(
+#     <cdashResultsUrlsStringOut>
+#     )
+#
+# Construct the build and test URLs on CDash given the site name, buildname,
+# and buildstamp (taken from the TAG file) from inside of a running ctest -S
+# program and optionally write it to a file as well.
+#
+function(tribits_get_cdash_results_urls_string  cdashResultsUrlsStringOut)
+  tribits_get_cdash_results_urls(
+    CDASH_BUILD_URL_OUT  cdashBuildUrl
+    CDASH_REVISION_BUILDS_URL_OUT  cdashRevisionBuildsUrl
+    CDASH_REVISION_NONPASSING_TESTS_URL_OUT  cdashRevisionNonpassingTestsUrl
+    )
+  tribits_generate_cdash_results_string_from_urls(
+    CDASH_BUILD_URL  "${cdashBuildUrl}"
+    CDASH_REVISION_BUILDS_URL  "${cdashRevisionBuildsUrl}"
+    CDASH_REVISION_NONPASSING_TESTS_URL  "${cdashRevisionNonpassingTestsUrl}"
+    CDASH_RESULTS_STRING_OUT  cdashResultsUrlsString
+    )
+  set(${cdashResultsUrlsStringOut} "${cdashResultsUrlsString}" PARENT_SCOPE)
+endfunction()
+
+
+# @FUNCTION: tribits_generate_cdash_results_string_from_urls()
+#
+# Generate the CDash results URL string given the individual URLs.
+#
+# Usage::
+#
+#   tribits_generate_cdash_results_string_from_urls(
+#     CDASH_BUILD_URL  "<cdashBuildUrl>"
+#     [CDASH_REVISION_BUILDS_URL  "<cdashRevisionBuildsUrl>"]
+#     [CDASH_REVISION_NONPASSING_TESTS_URL  "<cdashRevisionNonpassingTestsUrl>"]
+#     CDASH_RESULTS_STRING_OUT  <cdashResultsUrlsStringOut>
+#     )
+#
+# Takes the URLs returned from `tribits_get_cdash_results_urls()`_ and
+# generates a string out of them which is set in the return var
+# ``<cdashResultsUrlsStringOut>``.
+#
+function(tribits_generate_cdash_results_string_from_urls)
+  # Parse args
+  cmake_parse_arguments(PARSE_ARGV 0
+    PARSE "" "" # prefix, options, one_value_keywords
+    # multi_value_keywords
+    "CDASH_BUILD_URL;CDASH_REVISION_BUILDS_URL;CDASH_REVISION_NONPASSING_TESTS_URL;CDASH_RESULTS_STRING_OUT"
+    )
+  tribits_check_for_unparsed_arguments()
+  tribits_assert_parse_arg_one_value(PARSE  CDASH_BUILD_URL
+    CDASH_RESULTS_STRING_OUT)
+  tribits_assert_parse_arg_zero_or_one_value(PARSE  CDASH_REVISION_BUILDS_URL
+    CDASH_REVISION_NONPASSING_TESTS_URL)
+  # Construct CDash results URLs string
+  set(cdashResultsString "")
+  string(APPEND  cdashResultsString
+    "Link to this build's results on CDash:\n"
+    "\n"
+    "    ${PARSE_CDASH_BUILD_URL}\n")
+  if (PARSE_CDASH_REVISION_BUILDS_URL)
+    string(APPEND  cdashResultsString
+      "\nLink to all builds for this repo version on CDash:\n"
+      "\n"
+      "    ${PARSE_CDASH_REVISION_BUILDS_URL}\n")
+  endif()
+  if (PARSE_CDASH_REVISION_NONPASSING_TESTS_URL)
+    string(APPEND  cdashResultsString
+      "\nLink to all nonpassing tests for all builds for this repo version on CDash:\n"
+      "\n"
+      "    ${PARSE_CDASH_REVISION_NONPASSING_TESTS_URL}\n")
+  endif()
+  # Set output
+  set(${PARSE_CDASH_RESULTS_STRING_OUT} ${cdashResultsString} PARENT_SCOPE)
+endfunction()
+
+
+# @FUNCTION: tribits_get_cdash_results_urls()
+#
+# Construct the build and test URLs on CDash given the site name, buildname,
+# and buildstamp (taken from the TAG file) from inside of a running ctest -S
 # program and optionally write it to a file as well.
 #
 # Usage::
 #
-#   tribits_get_build_url_and_write_to_file(
-#     <cdashBuildUrlOut> [ <cdashBuldUrlFile> ] )
+#   tribits_get_cdash_results_urls(
+#     CDASH_BUILD_URL_OUT <cdashBuildUrlOut>
+#     [CDASH_REVISION_BUILDS_URL_OUT <cdashRevisionBuildsUrlOut>]
+#     [CDASH_REVISION_NONPASSING_TESTS_URL_OUT <cdashRevisionNonpassingTestsUrlOut>]
+#     )
+#
+# Here, the CDash URLs are constructed the following CMake variables already
+# set in a ``ctest -S`` process:
+#
+#   * ``CTEST_DROP_SITE``
+#   * ``CTEST_DROP_LOCATION`` (``submit.php`` is replaced with ``index.php``)
+#   * ``CTEST_PROJECT_NAME``
+#   * ``CTEST_SITE``
+#   * ``CTEST_BUILD_NAME``
+#   * ``CTEST_BINARY_DIRECTORY``
+#   * ``CTEST_SOURCE_DIRECTORY``
+#
+# and other information derived from that.
+#
+# The buildstamp is read in from the file
+# ``${CTEST_BINARY_DIRECTORY}/Testing/TAG``.
+#
+# If available, the revision SHA1 is obtained from the git repo at
+# ``CTEST_SOURCE_DIRECTORY`` if the directory
+# ``${CTEST_SOURCE_DIRECTORY}/.git`` exists.  If the base project source
+# directory is not a git reposistory, then ``<cdashRevisionBuildsUrlOut>`` and
+# ``<cdashRevisionNonpassingTestsUrlOut>``, if requested, will be set to
+# empty.
+#
+# Note that the CDash URLs will have ``https://`` added to the beginning so
+# that GitHub Actions and other systems will put in a hyperlink to them.
+#
+function(tribits_get_cdash_results_urls)
+  # Parse args
+  cmake_parse_arguments(PARSE_ARGV 0
+    PARSE "" "" # prefix, options, one_value_keywords
+    # multi_value_keywords
+    "CDASH_BUILD_URL_OUT;CDASH_REVISION_BUILDS_URL_OUT;CDASH_REVISION_NONPASSING_TESTS_URL_OUT"
+    )
+  tribits_check_for_unparsed_arguments(PARSE)
+  tribits_assert_parse_arg_one_value(PARSE  CDASH_BUILD_URL_OUT)
+  tribits_assert_parse_arg_zero_or_one_value(PARSE  CDASH_REVISION_BUILDS_URL_OUT
+    CDASH_REVISION_NONPASSING_TESTS_URL_OUT)
+  # Get the info
+  tribits_get_cdash_build_url(cdashBuildUrl)
+  tribits_git_repo_sha1("${CTEST_SOURCE_DIRECTORY}"  gitRepoSha1
+    FAILURE_MESSAGE_OUT  gitRepoSha1FailureMsg)
+  if (gitRepoSha1)
+    tribits_get_cdash_site_from_drop_site_and_location(
+      CTEST_DROP_SITE "${CTEST_DROP_SITE}"
+      CTEST_DROP_LOCATION "${CTEST_DROP_LOCATION}"
+      CDASH_SITE_URL_OUT  cdashSiteUrl
+      )
+    tribits_get_cdash_revision_builds_url(
+      CDASH_SITE_URL "${cdashSiteUrl}"
+      PROJECT_NAME "${CTEST_PROJECT_NAME}"
+      GIT_REPO_SHA1  "${gitRepoSha1}"
+      CDASH_REVISION_BUILDS_URL_OUT  cdashRevisionBuildsUrl
+     )
+   tribits_get_cdash_revision_nonpassing_tests_url(
+      CDASH_SITE_URL "${cdashSiteUrl}"
+      PROJECT_NAME "${CTEST_PROJECT_NAME}"
+      GIT_REPO_SHA1  "${gitRepoSha1}"
+      CDASH_REVISION_NONPASSING_TESTS_URL_OUT  cdashRevisionNonpassingTestsUrl
+      )
+  else()
+    set(cdashRevisionBuildsUrl "")
+    set(cdashRevisionNonpassingTestsUrl "")
+  endif()
+  # Set the outputs
+  set(${PARSE_CDASH_BUILD_URL_OUT} "${cdashBuildUrl}" PARENT_SCOPE)
+  set(${PARSE_CDASH_REVISION_BUILDS_URL_OUT} "${cdashRevisionBuildsUrl}"
+    PARENT_SCOPE)
+  set(${PARSE_CDASH_REVISION_NONPASSING_TESTS_URL_OUT} "${cdashRevisionNonpassingTestsUrl}"
+    PARENT_SCOPE)
+endfunction()
+
+
+# @FUNCTION: tribits_get_cdash_revision_builds_url()
+#
+# Get the CDash URL for all builds with the same repo version SHA1
+#
+# Usage::
+#
+#   tribits_get_cdash_revision_builds_url(
+#     CDASH_SITE_URL <cdashSiteUrl>
+#     PROJECT_NAME <projectName>
+#     GIT_REPO_SHA1 <gitRepoSha1>
+#     CDASH_REVISION_BUILDS_URL_OUT <cdashRevisionBuildsUrlOut>
+#     )
+#
+function(tribits_get_cdash_revision_builds_url)
+  cmake_parse_arguments(PARSE_ARGV 0
+    PARSE "" "" # prefix, options, one_value_keywords
+    # multi_value_keywords
+    "CDASH_SITE_URL;PROJECT_NAME;GIT_REPO_SHA1;CDASH_REVISION_BUILDS_URL_OUT"
+    )
+  tribits_check_for_unparsed_arguments()
+  tribits_assert_parse_arg_one_value(PARSE  CDASH_SITE_URL  PROJECT_NAME
+    GIT_REPO_SHA1  CDASH_REVISION_BUILDS_URL_OUT)
+  set(${PARSE_CDASH_REVISION_BUILDS_URL_OUT}
+    "${PARSE_CDASH_SITE_URL}/index.php?project=${PARSE_PROJECT_NAME}&filtercount=1&showfilters=1&field1=revision&compare1=61&value1=${PARSE_GIT_REPO_SHA1}"
+    PARENT_SCOPE)
+endfunction()
+
+
+# @FUNCTION: tribits_get_cdash_revision_nonpassing_tests_url()
+#
+# Get the CDash URL for all non-passing tests with the same repo version SHA1
+#
+# Usage::
+#
+#   tribits_get_cdash_revision_nonpassing_tests_url(
+#     CDASH_SITE_URL <cdashSiteUrl>
+#     PROJECT_NAME <projectName>
+#     GIT_REPO_SHA1 <gitRepoSha1>
+#     CDASH_REVISION_NONPASSING_TESTS_URL_OUT <cdashRevisionNonpassingTestsUrlOut>
+#     )
+#
+function(tribits_get_cdash_revision_nonpassing_tests_url)
+  cmake_parse_arguments( PARSE_ARGV 0
+    PARSE "" "" # prefix, options, one_value_keywords
+    # multi_value_keywords
+    "CDASH_SITE_URL;PROJECT_NAME;GIT_REPO_SHA1;CDASH_REVISION_NONPASSING_TESTS_URL_OUT"
+    )
+  tribits_check_for_unparsed_arguments()
+  tribits_assert_parse_arg_one_value(PARSE  CDASH_SITE_URL  PROJECT_NAME
+    GIT_REPO_SHA1  CDASH_REVISION_NONPASSING_TESTS_URL_OUT)
+  set(${PARSE_CDASH_REVISION_NONPASSING_TESTS_URL_OUT}
+    "${PARSE_CDASH_SITE_URL}/queryTests.php?project=${PARSE_PROJECT_NAME}&filtercount=2&showfilters=1&filtercombine=and&field1=revision&compare1=61&value1=${PARSE_GIT_REPO_SHA1}&field2=status&compare2=62&value2=passed"
+    PARENT_SCOPE)
+endfunction()
+
+
+# @FUNCTION: tribits_get_cdash_build_url()
+#
+# Construct the build URL on CDash given the site name, buildname, and
+# buildstamp (taken from the TAG file) from inside of a running ctest -S
+# program.
+#
+# Usage::
+#
+#   tribits_get_cdash_build_url(<cdashBuildUrlOut>)
 #
 # Here, ``<cdashBuildUrlOut>`` returns the CDash Build URL constructed from
 # the following CMake variables already set in a ``ctest -S`` process:
@@ -70,10 +334,7 @@ include(TribitsReadTagFile)
 # beginning of it so that GitHub Actions and other systems will put in a link
 # to them.
 #
-# If the file name argument ``<cdashBuldUrlFile>`` is non-empty, then that
-# CDash URL will be written to the file as a single line.
-#
-function(tribits_get_build_url_and_write_to_file  cdashBuildUrlOut  cdashBuildUrlFile)
+function(tribits_get_cdash_build_url  cdashBuildUrlOut)
   tribits_get_cdash_index_php_from_drop_site_and_location(
     CTEST_DROP_SITE "${CTEST_DROP_SITE}"
     CTEST_DROP_LOCATION "${CTEST_DROP_LOCATION}"
@@ -87,10 +348,6 @@ function(tribits_get_build_url_and_write_to_file  cdashBuildUrlOut  cdashBuildUr
     TAG_FILE "${CTEST_BINARY_DIRECTORY}/Testing/TAG"
     CDASH_BUILD_URL_OUT cdashBuildUrl
     )
-  set(cdashBuildUrl "${cdashBuildUrl}")
-  if (cdashBuildUrlFile)
-    file(WRITE "${cdashBuildUrlFile}" "${cdashBuildUrl}")
-  endif()
   set(${cdashBuildUrlOut} "${cdashBuildUrl}" PARENT_SCOPE)
 endfunction()
 
