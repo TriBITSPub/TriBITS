04ea94c4a86f6e385cd6c63a723bfc6c5d0ac69b
bartlettroscoe <rabartl@sandia.gov>
2022-11-04 20:22:11 +0000

    Deploying to deploy-doc-site from @ TriBITSPub/TriBITS@be33367df373f8ec9389b22a240342b68839805f 🚀

--- build_ref/_static/documentation_options.js
+++ build_ref/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'tribits_start-3106-g7e686376',
+    VERSION: 'tribits_start-3108-gbe33367d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
