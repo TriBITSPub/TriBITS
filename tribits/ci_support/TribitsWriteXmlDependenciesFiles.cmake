432a316772438178e408f0c333a3e3285fab35d2
Roscoe A. Bartlett <rabartl@sandia.gov>
2022-11-22 14:23:27 -0700

    WIP: Reorder functions in clean code ordering (#63)
    Otherwise, I just updated a few comments.  But the code is unchanged.

--- tribits/ci_support/TribitsWriteXmlDependenciesFiles.cmake
+++ tribits/ci_support/TribitsWriteXmlDependenciesFiles.cmake
@@ -38,111 +38,11 @@
 # @HEADER
 
 
-#
-# This file gets included in the main TriBITS framework.  It is put here to
-# reduce the size of the tribits/core/ directory.
-#
-
-
-function(tribits_write_deps_to_xml_string  PACKAGE_NAME  LIST_TYPE
-  XML_VAR
-  )
-
-  set(LOC_XML "${${XML_VAR}}")
-
-  set(DEPS_VAR ${PACKAGE_NAME}_${LIST_TYPE})
-  assert_defined(DEPS_VAR)
-  set(DEPS ${${DEPS_VAR}})
-
-  #print_var(PACKAGE_NAME)
-  #print_var(DEPS)
-
-  if (NOT DEPS)
-
-    list(APPEND LOC_XML
-      "    <${LIST_TYPE}/>\n" )
-
-  else()
-
-    set(VALUE_STR "")
-
-    foreach(DEP ${DEPS})
-
-      if(VALUE_STR)
-        set(VALUE_STR "${VALUE_STR},")
-      endif()
-
-      set(VALUE_STR "${VALUE_STR}${DEP}")
-
-    endforeach()
-
-    list(APPEND LOC_XML
-      "    <${LIST_TYPE} value=\"${VALUE_STR}\"/>\n" )
-
-  endif()
-
-  if (LOC_XML)
-    set(${XML_VAR} "${LOC_XML}" PARENT_SCOPE)
-  endif()
-
-endfunction()
-
-
-#
-# Function that writes the dependency information for ${PROJECT_NAME} into
-# an XML file for other tools to use.
-#
-
-function(tribits_dump_deps_xml_file)
-
-  set(DEPS_XM)
-
-  get_filename_component(PROJECT_BASE_DIR_NAME  "${${PROJECT_NAME}_SOURCE_DIR}"  NAME)
-
-  list(APPEND DEPS_XML
-    "<PackageDependencies project=\"${PROJECT_NAME}\" baseDirName=\"${PROJECT_BASE_DIR_NAME}\">\n"
-     )
-
-  foreach(TRIBITS_PACKAGE ${${PROJECT_NAME}_DEFINED_INTERNAL_PACKAGES})
-
-    #message("")
-    #print_var(TRIBITS_PACKAGE)
-
-    list(APPEND DEPS_XML
-      "  <Package name=\"${TRIBITS_PACKAGE}\" dir=\"${${TRIBITS_PACKAGE}_REL_SOURCE_DIR}\" type=\"${${TRIBITS_PACKAGE}_TESTGROUP}\">\n")
-
-    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} LIB_REQUIRED_DEP_PACKAGES DEPS_XML)
-    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} LIB_OPTIONAL_DEP_PACKAGES DEPS_XML)
-    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} TEST_REQUIRED_DEP_PACKAGES DEPS_XML)
-    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} TEST_OPTIONAL_DEP_PACKAGES DEPS_XML)
-    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} LIB_REQUIRED_DEP_TPLS DEPS_XML)
-    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} LIB_OPTIONAL_DEP_TPLS DEPS_XML)
-    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} TEST_REQUIRED_DEP_TPLS DEPS_XML)
-    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} TEST_OPTIONAL_DEP_TPLS DEPS_XML)
-
-    list(APPEND DEPS_XML
-      "    <EmailAddresses>\n"
-      "      <Regression address=\"${${TRIBITS_PACKAGE}_REGRESSION_EMAIL_LIST}\"/>\n"
-      "    </EmailAddresses>\n"
-      )
-
-    list(APPEND DEPS_XML
-      "    <ParentPackage value=\"${${TRIBITS_PACKAGE}_PARENT_PACKAGE}\"/>\n"
-      )
-
-    list(APPEND DEPS_XML
-      "  </Package>\n" )
-
-  endforeach()
-
-  list(APPEND DEPS_XML
-    "</PackageDependencies>\n" )
-
-  #print_var(DEPS_XML)
-  string(REPLACE "\n;" "\n" DEPS_XML "${DEPS_XML}")
-  file(WRITE ${${PROJECT_NAME}_DEPS_XML_OUTPUT_FILE} "${DEPS_XML}" )
-
-endfunction()
+################################################################################
+# NOTE: This file gets included in the main TriBITS core framework.  It is put
+# here to reduce the size of the tribits/core/ directory as this is optional
+# behavior.
+################################################################################
 
 
 # @MACRO: tribits_write_xml_dependency_files()
@@ -151,8 +51,8 @@ endfunction()
 #
 #   tribits_write_xml_dependency_files()
 #
-# Macro that output XML dependency files if asked based in the global project
-# package dependency graph previously constructed..
+# Macro that outputs XML dependency files if asked based in the global project
+# package dependency graph previously constructed.
 #
 macro(tribits_write_xml_dependency_files)
 
@@ -207,3 +107,102 @@ macro(tribits_write_xml_dependency_files)
   endif()
 
 endmacro()
+
+
+# Function that writes the dependency information for the TriBITS project
+# ${PROJECT_NAME} into an XML file for other tools to use.
+#
+function(tribits_dump_deps_xml_file)
+
+  set(DEPS_XM)
+
+  get_filename_component(PROJECT_BASE_DIR_NAME  "${${PROJECT_NAME}_SOURCE_DIR}"  NAME)
+
+  list(APPEND DEPS_XML
+    "<PackageDependencies project=\"${PROJECT_NAME}\" baseDirName=\"${PROJECT_BASE_DIR_NAME}\">\n"
+     )
+
+  foreach(TRIBITS_PACKAGE ${${PROJECT_NAME}_DEFINED_INTERNAL_PACKAGES})
+
+    #message("")
+    #print_var(TRIBITS_PACKAGE)
+
+    list(APPEND DEPS_XML
+      "  <Package name=\"${TRIBITS_PACKAGE}\" dir=\"${${TRIBITS_PACKAGE}_REL_SOURCE_DIR}\" type=\"${${TRIBITS_PACKAGE}_TESTGROUP}\">\n")
+
+    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} LIB_REQUIRED_DEP_PACKAGES DEPS_XML)
+    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} LIB_OPTIONAL_DEP_PACKAGES DEPS_XML)
+    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} TEST_REQUIRED_DEP_PACKAGES DEPS_XML)
+    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} TEST_OPTIONAL_DEP_PACKAGES DEPS_XML)
+    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} LIB_REQUIRED_DEP_TPLS DEPS_XML)
+    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} LIB_OPTIONAL_DEP_TPLS DEPS_XML)
+    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} TEST_REQUIRED_DEP_TPLS DEPS_XML)
+    tribits_write_deps_to_xml_string(${TRIBITS_PACKAGE} TEST_OPTIONAL_DEP_TPLS DEPS_XML)
+
+    list(APPEND DEPS_XML
+      "    <EmailAddresses>\n"
+      "      <Regression address=\"${${TRIBITS_PACKAGE}_REGRESSION_EMAIL_LIST}\"/>\n"
+      "    </EmailAddresses>\n"
+      )
+
+    list(APPEND DEPS_XML
+      "    <ParentPackage value=\"${${TRIBITS_PACKAGE}_PARENT_PACKAGE}\"/>\n"
+      )
+
+    list(APPEND DEPS_XML
+      "  </Package>\n" )
+
+  endforeach()
+
+  list(APPEND DEPS_XML
+    "</PackageDependencies>\n" )
+
+  #print_var(DEPS_XML)
+  string(REPLACE "\n;" "\n" DEPS_XML "${DEPS_XML}")
+  file(WRITE ${${PROJECT_NAME}_DEPS_XML_OUTPUT_FILE} "${DEPS_XML}" )
+
+endfunction()
+
+
+function(tribits_write_deps_to_xml_string  PACKAGE_NAME  LIST_TYPE
+  XML_VAR
+  )
+
+  set(LOC_XML "${${XML_VAR}}")
+
+  set(DEPS_VAR ${PACKAGE_NAME}_${LIST_TYPE})
+  assert_defined(DEPS_VAR)
+  set(DEPS ${${DEPS_VAR}})
+
+  #print_var(PACKAGE_NAME)
+  #print_var(DEPS)
+
+  if (NOT DEPS)
+
+    list(APPEND LOC_XML
+      "    <${LIST_TYPE}/>\n" )
+
+  else()
+
+    set(VALUE_STR "")
+
+    foreach(DEP ${DEPS})
+
+      if(VALUE_STR)
+        set(VALUE_STR "${VALUE_STR},")
+      endif()
+
+      set(VALUE_STR "${VALUE_STR}${DEP}")
+
+    endforeach()
+
+    list(APPEND LOC_XML
+      "    <${LIST_TYPE} value=\"${VALUE_STR}\"/>\n" )
+
+  endif()
+
+  if (LOC_XML)
+    set(${XML_VAR} "${LOC_XML}" PARENT_SCOPE)
+  endif()
+
+endfunction()
