957794e9944df518955efccf824c0848ec144d16
Roscoe A. Bartlett <rabartl@sandia.gov>
2024-07-26 19:51:13 -0600

    Add TriBITS test drivers for cee-rhel8 machines with AUE modules

---
+++ dev_testing/cee-rhel8/load-env.clang-16.0.6.sh
@@ -0,0 +1,7 @@
+module purge
+module load aue/cmake/3.27.7
+module load aue/ninja/1.11.1
+module load aue/clang/16.0.6
+module load aue/openmpi/4.1.6-clang-16.0.6
+
+export TribitsExMetaProj_GIT_URL_REPO_BASE=git@github.com:tribits/
