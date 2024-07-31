64436bd663b9626d8b0d65a73af0c7f8f53b71d0
Roscoe A. Bartlett <rabartl@sandia.gov>
2024-07-31 10:55:34 -0600

    Use locally installed codespell for CEE RHEL8 machines
    I installed this with:
    
      pip3 install --user codespell

--- dev_testing/cee-rhel8/load-env.clang-16.0.6.sh
+++ dev_testing/cee-rhel8/load-env.clang-16.0.6.sh
@@ -4,4 +4,5 @@ module load aue/ninja/1.11.1
 module load aue/clang/16.0.6
 module load aue/openmpi/4.1.6-clang-16.0.6
 
+export PATH=${HOME}/.local/bin:${PATH}
 export TribitsExMetaProj_GIT_URL_REPO_BASE=git@github.com:tribits/
